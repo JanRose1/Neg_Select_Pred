29928043c6d7e458414a8d8b1e3acd4bfc750750
Jan Rosa <janrose@umich.edu>
2023-02-08 10:28:04 -0500

    Lots of stuff I haven't committed for some time

---
+++ setup.py
@@ -0,0 +1,7 @@
+
+from setuptools import setup
+
+setup(name = "MainCode", version = "0.1.0", packages = ["MainCode"])
+setup_keywords = ('distclass', 'script_name', 'script_args', 'options', 'name', 'version', 'author', 
+'author_email', 'maintainer', 'maintainer_email','url','license','description', 'long_description', 
+'keywords', 'platforms', 'classifiers', 'download_url', 'requires','provides','obsoletes')
