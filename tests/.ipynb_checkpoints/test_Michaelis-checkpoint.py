9e477a5df4bbc73d27b02181154d545bce1c0dbd
Jan Rosa <69280191+JanRose1@users.noreply.github.com>
2023-04-11 08:43:15 -0400

    Delete tests/.ipynb_checkpoints directory
    Deleted Checkpoint

--- tests/.ipynb_checkpoints/test_Michaelis-checkpoint.py
+++
@@ -1,8 +0,0 @@
-from Neg_Sel_Pred.negative_sel_project import Michaelis_Menten
-
-def test_0_Subtrate():
-	K_m = 1
-	S = 0
-	Vmax = 10
-	V = Michaelis_Menten(Vmax,S,K_m)
-	assert V == 0, "The expected Value should be 0"	
